872b4b05f6fa191289993d6b7c07d2393f08ea08
oliveiratamiris <tamirisocoelho@gmail.com>
2023-07-06 16:34:34 -0300

    Finalizado o exercício de média 2 do uri

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,33 @@
 package org.example;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Locale.setDefault(Locale.US);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        double a, b, c, mediaPoderada;
+
+        a = sc.nextDouble();
+
+        b = sc.nextDouble();
+
+        c = sc.nextDouble();
+
+        if((a >= 0.0 ) &&  (a <= 10.0) && (b >= 0.0 ) &&  (b <= 10.0) && (c >= 0.0 ) &&  (c <= 10.0)){
+            mediaPoderada = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+            System.out.printf("MEDIA = %.1f%n", mediaPoderada);
+        }else{
+            System.out.println("Nota inválida");
         }
+
+
+        sc. close();
     }
 }
